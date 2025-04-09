e8191d233722d3f5f011b652b997bc716cc01bb9
nsamuels <n-samuels@outlook.com>
2025-04-09 08:16:48 +0100

    player turn TS logic

--- tic-tac-toe/src/main.ts
+++ tic-tac-toe/src/main.ts
@@ -1,5 +1,12 @@
 import './style.scss'
 
-const tile = document.querySelectorAll<HTMLButtonElement>(".game__tile")
 
-button.addEventListener("click, func")
+// Step1: Capturing elements on the elements on the DOM
+const tile = document.querySelectorAll<HTMLButtonElement>(".game__tile")
+const turn = document.querySelector<HTMLDivElement>(".game__turn")
+
+if(!turn){
+throw new Error ("Do not have turn element")
+}
+turn.innerText = "Player X's Turn TS";
+// button.addEventListener("click, func")

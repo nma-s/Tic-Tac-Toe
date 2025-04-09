9e2c904046a781d332438cd58b30a38cabee1106
nsamuels <n-samuels@outlook.com>
2025-04-09 09:36:39 +0100

    TS first eventlistner for tiles

--- tic-tac-toe/src/main.ts
+++ tic-tac-toe/src/main.ts
@@ -2,7 +2,7 @@ import './style.scss'
 
 
 // Step1: Capturing elements on the elements on the DOM
-const tile = document.querySelectorAll<HTMLButtonElement>(".game__tile")
+const tiles = document.querySelectorAll<HTMLButtonElement>(".game__tile")
 const turn = document.querySelector<HTMLDivElement>(".game__turn")
 
 if(!turn){
@@ -10,3 +10,19 @@ throw new Error ("Do not have turn element")
 }
 turn.innerText = "Player X's Turn TS";
 // button.addEventListener("click, func")
+console.dir(tiles);
+
+// Add event Listener for to add functionality to each click of 
+// a. tile
+tiles.forEach((tile) => {
+    tile.addEventListener('click', function);
+})
+
+// function when each tile is clicked
+const tileClickEvents = ()=> {
+    // statement management - what state si it currently in
+
+    // on each click display the current player
+
+    
+}

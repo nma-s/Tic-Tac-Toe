f2c8dae4383e460d86b006939804ff958ec8b049
nsamuels <n-samuels@outlook.com>
2025-04-10 16:54:36 +0100

    changed HTML buttons to divs, moved turn display outside game container and changed grid styling

--- tic-tac-toe/src/main.ts
+++ tic-tac-toe/src/main.ts
@@ -5,24 +5,27 @@ import './style.scss'
 const tiles = document.querySelectorAll<HTMLButtonElement>(".game__tile")
 const turn = document.querySelector<HTMLDivElement>(".game__turn")
 
-if(!turn){
-throw new Error ("Do not have turn element")
+if(!turn || !tiles){
+throw new Error ("Do not have element")
 }
 turn.innerText = "Player X's Turn TS";
 // button.addEventListener("click, func")
 console.dir(tiles);
 
+// function when each tile is clicked
+const tileClickEvents = (event: Event)=> {
+    // statement management - what state si it currently in
+
+    const currentTile = event.currenttarget as HTMLButtonElement;
+    if (currentTile.textContent !== '') return;
+    // on each click display the current player
+    currentTile.textContent = 'X';   
+}
+
+
+
 // Add event Listener for to add functionality to each click of 
 // a. tile
 tiles.forEach((tile) => {
-    tile.addEventListener('click', function);
+    tile.addEventListener('click', tileClickEvents);
 })
-
-// function when each tile is clicked
-const tileClickEvents = ()=> {
-    // statement management - what state si it currently in
-
-    // on each click display the current player
-
-    
-}

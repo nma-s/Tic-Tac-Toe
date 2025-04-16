dc9711be4de6882a9c8278b38ced48d6a47ead3d
nsamuels <n-samuels@outlook.com>
2025-04-16 22:39:46 +0100

    winning combinations

--- tic-tac-toe/src/main.ts
+++ tic-tac-toe/src/main.ts
@@ -4,22 +4,26 @@ import './style.scss'
 // Capturing elements on the elements on the DOM
 const tiles = document.querySelectorAll<HTMLDivElement>(".game__tile");
 const turn = document.querySelector<HTMLDivElement>(".turn-display");
+const gameboard = document.querySelector<HTMLDivElement>(".game");
+const xScore = document.querySelector<HTMLDivElement>(".score__player--x");
+const oScore = document.querySelector<HTMLDivElement>(".score__player--o")
 
-// const gameBoard: sting[][] = [
-//     [0, 0], [0, 1], [0, 2], //top row
-//     [1, 0], [1, 1], [1, 2],
-//     [2, 0], [2, 1], [2, 2] // bottom row
-// ];
-
-// const tilePositions: number[][] = [
-//     [0, 0], [0, 1], [0, 2], //top row
-//     [1, 0], [1, 1], [1, 2],
-//     [2, 0], [2, 1], [2, 2] // bottom row
-// ];
+const winningCombinations: number[][] = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 let currentPlayer: string = 'X';
+let xScoreValue = 0;
 
-if(!turn || !tiles){
+
+if(!turn || !tiles || !gameboard || !xScore || !oScore){
     throw new Error (`There's an issue with one of the selectors: 
     tiles => ${tiles}, turn display => ${turn}`);
 }
@@ -41,6 +45,12 @@ const tileClickEvents = (event: Event) => {
   currentTile.innerText = currentPlayer;
 
   // check for win
+  if (checkWin(currentPlayer)) {
+    if(currentPlayer === 'X'){
+      xScoreValue++;
+      xScore.innerText = xScoreValue.toString();
+    }
+  }
   // check for draw
 
   // on each click check current player and switch
@@ -54,11 +64,22 @@ const tileClickEvents = (event: Event) => {
   turn.innerText = `It's Player ${currentPlayer}'s turn`;
 }
 
+const checkWin = (currentPlayer: string): boolean => {
+  for (let i = 0; i < winningCombinations.length){
+    const [a , b , c] = winningCombinations[i];
+    if(tiles[a].innerHTML === currentPlayer && 
+      tiles[b].innerHTML === currentPlayer && 
+      tiles[c].innerHTML === currentPlayer) {
+        return true
+      }
+  }
+  return false;
+}
+
 
 
 // Add event Listener for to add functionality to each click of 
 // a. tile
 tiles.forEach((tile, index) => {
-    // const [row, col] = tilePositions[index];
     tile.addEventListener('click', tileClickEvents, { once: true });
 })
